48ac658467ca99c2b1aff8ffbce0ea09f8261fd9
Sayman Rifat <saymanrifat@gmail.com>
2023-12-22 12:58:01 +0600

    Initial Commit

---
+++ RestaurantApp/RestaurantAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantAppApp.swift
+//  RestaurantApp
+//
+//  Created by Sayman Rifat on 12/22/23.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
